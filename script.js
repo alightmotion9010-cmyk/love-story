3360224899ebca3ab576db2ee23a3a5494e2ce41
alightmotion9010-cmyk <alightmotion9010@gmail.com>
2026-06-26 04:30:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+/*=============================
+LOADING
+=============================*/
+
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+document.getElementById("loading").style.display="none";
+
+},1800);
+
+});
+
+/*=============================
+MUSIC
+=============================*/
+
+const music=document.getElementById("bgMusic");
+
+const enterBtn=document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click",()=>{
+
+music.play();
+
+document.querySelector(".opening").scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+/*=============================
+POPUP
+=============================*/
+
+const popup=document.getElementById("letterPopup");
+
+const openBtn=document.getElementById("letterBtn");
+
+const closeBtn=document.getElementById("closeLetter");
+
+openBtn.onclick=()=>{
+
+popup.style.display="flex";
+
+}
+
+closeBtn.onclick=()=>{
+
+popup.style.display="none";
+
+}
+
+window.onclick=(e)=>{
+
+if(e.target==popup){
+
+popup.style.display="none";
+
+}
+
+}
+
+/*=============================
+SCROLL ANIMATION
+=============================*/
+
+const hidden=document.querySelectorAll(".hidden");
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+hidden.forEach(el=>observer.observe(el));
 
